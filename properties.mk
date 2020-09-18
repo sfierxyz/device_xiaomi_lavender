1c0f2e339777b42ab491c6ada1d3356f64ce09d1
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:57:23 -0700

    lavender: Enable compositor background blurring
    This allows us to create a "glass blur" look throughout the system for
    improved visuals.
    
    Change-Id: I77bb13a5711b05b43acf25e12abd9a2f5cd2502d

--- properties.mk
+++ properties.mk
@@ -23,3 +23,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.use_color_management=true
+
+# Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1
