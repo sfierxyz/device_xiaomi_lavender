1a5004631943b3bafc352c40d8e2470f069b7f65
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:58:27 -0700

    lavender: Use EXPENSIVE_RENDERING power hint for blurs
    We need to boost the GPU for smooth high-radius blurs as blurring is an
    expensive rendering job.
    
    Change-Id: Ia3968313601d2b04ce50c01ee61dc9944ee26187

--- properties.mk
+++ properties.mk
@@ -26,4 +26,5 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Blur
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
