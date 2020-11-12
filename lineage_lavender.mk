92a76eab6139338b9c581a4172800502a07e8715
VerpaPhullus <niktofe1@gmail.com>
2020-11-12 16:26:17 +1000

    lavender: update build_fingerprint to last redfin

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -38,4 +38,4 @@ TARGET_VENDOR_PRODUCT_NAME := lavender
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.6.0.PFGMIXM release-keys"
 
-BUILD_FINGERPRINT := xiaomi/lavender/lavender:9/PKQ1.180904.001/V10.3.6.0.PFGMIXM:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003.A1/6886512:user/release-keys
