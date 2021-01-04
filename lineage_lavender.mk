fb6846d6c1419a6746f18f3d7ffdaa706c3aef67
DillerOFire <niktofe1@gmail.com>
2021-01-04 22:27:53 +1000

    lavender: fill fluid related props
    Signed-off-by: DillerOFire <niktofe1@gmail.com>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -49,3 +49,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.6.0.PFGMIXM release-keys"
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=Sfier Reichenbach \
+  ro.fluid.cpu=SDM660
