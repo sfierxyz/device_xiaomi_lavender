746bb8690747a1ed43217e8a13a4aa7b7dc52ea9
DillerOFire <niktofe1@gmail.com>
2021-02-01 09:06:41 +0300

    lavender: AOSiP-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/aosip_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    aosip_lavender-user \
+    aosip_lavender-userdebug \
+    aosip_lavender-eng
